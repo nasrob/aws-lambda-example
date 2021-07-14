fcb49df378626be437b40edea2afdc0479aa16b1
Nasser BOUKEHIL <boukehil.nasreddine@gmail.com>
2021-07-14 22:26:25 +0200

    fixed typo

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const sum = require('lodash.sum')
 
-export.handler = async event => {
+exports.handler = async event => {
     const response = {
         statusCode: 200,
         body: JSON.stringify({
-            message: 'Hello From Lambda!',
-            sum: sum([1, 2, 3])
+        message: 'Hello from Lambda!',
+        sum: sum([1, 2, 3])
         })
     }
     return response
